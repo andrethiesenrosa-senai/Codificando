76a40a92b4497816d6288875759a140e75958a80
andrethiesenrosa-senai <andre.thiesen.rosa@edu.senai.sc.br>
2026-05-19 21:25:39 -0300

    Primeira Classe

---
+++ Assets/Scripts/Cachorros.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Cachorros : MonoBehaviour
+{
+    public string nome;
+
+    
+    private void Start()
+    {
+        Cachorros pitbul = new Cachorros();
+        pitbul.nome = "Bob";
+    }
+}
