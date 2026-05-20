e313b3b51703eb1f77b1b50bc8c439a4a98c4d2d
andrethiesenrosa-senai <andre.thiesen.rosa@edu.senai.sc.br>
2026-05-19 21:31:34 -0300

    Atributo sono adicionado.

--- Assets/Scripts/Cachorros.cs
+++ Assets/Scripts/Cachorros.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class Cachorros : MonoBehaviour
 {
     public string nome;
+    public bool sono;
 
     
     private void Start()
     {
         Cachorros pitbul = new Cachorros();
         pitbul.nome = "Bob";
+        pitbul.sono = true;
     }
 }
